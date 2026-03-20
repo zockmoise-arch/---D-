ea6eafd3f15468561e63e6b9f87ce30781539de5
zockmoise-arch <zock_moise@icloud.com>
2026-03-20 01:10:26 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+  BotName: "𝐙𝐊-𝐁𝐎𝐓", //Bot name 
+  nameCreator: "𝐙𝐊-𝐌D ", // your name 
+  numberCreator: "237673642385", //your number 
+  Newsletter: "120363401981326656@newsletter",//newsletter for your channel
+  
